fca56175c10db0aabaf97d345bf311b00acae934
Brandon <brandon@tendency.me>
2024-05-04 16:55:15 -0500

    chore: improve implementation

--- src/Concerns/Tools/HasProfileDefaults.php
+++ src/Concerns/Tools/HasProfileDefaults.php
@@ -4,60 +4,49 @@ namespace Awcodes\Scribble\Concerns\Tools;
 
 use Awcodes\Scribble\Profiles\DefaultProfile;
 use Closure;
+use Illuminate\Support\Arr;
 
 trait HasProfileDefaults
 {
-    protected array | Closure $bubbleTool = [];
+    protected string | array | Closure $bubbleTool = [];
 
-    protected array | Closure $suggestionTool = [];
+    protected string | array | Closure $suggestionTool = [];
 
-    protected array | Closure $toolbarTool = [];
+    protected string | array | Closure $toolbarTool = [];
 
     public function bubbleTool(string | array | Closure $profiles = []): static
     {
-        if (is_string($profiles)) {
-            $profiles = [$profiles];
-        }
-
-        $this->bubbleTool = $profiles ?: [DefaultProfile::class];
+        $this->bubbleTool = $profiles ?: DefaultProfile::class;
 
         return $this;
     }
 
     public function suggestionTool(string | array | Closure $profiles = []): static
     {
-        if (is_string($profiles)) {
-            $profiles = [$profiles];
-        }
-
-        $this->suggestionTool = $profiles ?: [DefaultProfile::class];
+        $this->suggestionTool = $profiles ?: DefaultProfile::class;
 
         return $this;
     }
 
     public function toolbarTool(string | array | Closure $profiles = []): static
     {
-        if (is_string($profiles)) {
-            $profiles = [$profiles];
-        }
-
-        $this->toolbarTool = $profiles ?: [DefaultProfile::class];
+        $this->toolbarTool = $profiles ?: DefaultProfile::class;
 
         return $this;
     }
 
     public function getBubbleTool(): array
     {
-        return $this->evaluate($this->bubbleTool) ?? [];
+        return Arr::wrap($this->evaluate($this->bubbleTool) ?? []);
     }
 
     public function getSuggestionTool(): array
     {
-        return $this->evaluate($this->suggestionTool) ?? [];
+        return Arr::wrap($this->evaluate($this->suggestionTool) ?? []);
     }
 
     public function getToolbarTool(): array
     {
-        return $this->evaluate($this->toolbarTool) ?? [];
+        return Arr::wrap($this->evaluate($this->toolbarTool) ?? []);
     }
 }
